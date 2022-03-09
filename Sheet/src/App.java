ffce7a95f9851da63e71bda5d5c73df05197150c
Piyush <piyushat25@gmail.com>
2022-03-09 19:09:05 +0530

    rotated array

---
+++ Sheet/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        RotateArray obj = new RotateArray();
+        int[] nums = new int[] { -1, -100, 3, 99 };
+        int k = 2;
+        obj.rotate(nums, k);
+    }
+}
